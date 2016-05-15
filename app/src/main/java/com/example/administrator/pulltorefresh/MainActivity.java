7839181d67ad626112a24c4eecf5ae4e1e6f1a9d
Patrick <495130561@qq.com>
2016-05-15 20:13:59 +0800

    add SlidingMenu

--- app/src/main/java/com/example/administrator/pulltorefresh/MainActivity.java
+++ app/src/main/java/com/example/administrator/pulltorefresh/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.pulltorefresh;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,14 +11,16 @@ import android.widget.ListView;
 
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, PullToRefreshBase.OnRefreshListener {
 
     private PullToRefreshListView pullToRefreshListView;
+    private SlidingMenu slidingMenu;
     private MyAdapter myAdapter;
     private List<String> mData;
     private ImageView ivMenu;
@@ -27,33 +30,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initUi();
+        initData();
+    }
+
+    private void initUi(){
         pullToRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_to_refresh_listview);
-        pullToRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
-            @Override
-            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
-                Log.v("aaaa", "aaaa");
-                new GetDataTask().execute();
-            }
-        });
-
         ivMenu = (ImageView) findViewById(R.id.sildingmenu);
-        ivMenu.setOnClickListener(this);
+        slidingMenu = new SlidingMenu(this);
+    }
 
+    private void initData(){
+        pullToRefreshListView.setOnRefreshListener(this);
+        ivMenu.setOnClickListener(this);
         mData = new ArrayList<>();
         mData.add("111");
         mData.add("222");
         myAdapter = new MyAdapter(this, mData);
         pullToRefreshListView.setAdapter(myAdapter);
 
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        slidingMenu.setTouchModeAbove(SlidingMenu.LEFT);
+        slidingMenu.setShadowWidthRes(R.dimen.shadow_width);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        slidingMenu.setBehindWidth(dm.widthPixels / 3);
+        slidingMenu.setFadeDegree(0.35f);
+        slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        slidingMenu.setMenu(R.layout.layout_left_menu);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.sildingmenu:
-                Log.v("aaa", "bbb");
+                slidingMenu.showMenu();
                 break;
         }
+    }
+
+    @Override
+    public void onRefresh(PullToRefreshBase refreshView) {
+        new GetDataTask().execute();
     }
 
     private class GetDataTask extends AsyncTask<Void, Void, String[]>{
@@ -61,7 +79,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         protected String[] doInBackground(Void... params) {
             try {
-                Log.v("aaaa","in");
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
